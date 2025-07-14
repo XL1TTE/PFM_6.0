6d50db579a6e2c52ad49b4948afe5e3ad487f710
XL1TTE <zzzxl1tteqqq@mail.com>
2025-07-14 13:07:27 +0700

    battle field components added

--- Assets/Project/ECS/Components/BattleFieldCells/BattleFieldCellPosition.cs
+++ Assets/Project/ECS/Components/BattleFieldCells/BattleFieldCellPosition.cs
@@ -4,7 +4,10 @@ using Scellecs.Morpeh;
 namespace ECS.Components{
     [Serializable]
     public struct BattleFieldCellPosition: IComponent{
-        public int x;
-        public int y; 
+        public int grid_x;
+        public int grid_y;
+        
+        public int global_x;
+        public int global_y; 
     }
 }
